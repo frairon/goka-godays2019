d280af2bab1894abcbc5bf63fc673052bf3df863
frairon <franz.eichhorn@lovoo.com>
2019-01-28 11:01:22 +0100

    topic comments

--- topics.go
+++ topics.go
@@ -3,7 +3,11 @@ package godays
 import "github.com/lovoo/goka"
 
 const (
-	TripStartedTopic   goka.Stream = "trip-started"
-	TripEndedTopic     goka.Stream = "trip-ended"
+	// TripStartedTopic contains the stream for pickup-events
+	TripStartedTopic goka.Stream = "trip-started"
+	// TripEndedTopic contains the stream for dropoff-events
+	TripEndedTopic goka.Stream = "trip-ended"
+
+	// LicenseConfigTopic (for task 3 and 4) to configure licenses
 	LicenseConfigTopic goka.Stream = "configure-licenses"
 )
